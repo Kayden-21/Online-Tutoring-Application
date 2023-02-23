063fb0cfc2f72e8af98af68715904379d2bb4367
i-renaissant <hewitt.tny@gmail.com>
2023-02-23 20:44:29 +0200

    modify functions

--- Create_functions.sql
+++ Create_functions.sql
@@ -1,4 +1,9 @@
 --  1. Get Number of Students in Booking
+
+IF OBJECT_ID (N'NumberOfStudentInBooking', N'FN') IS NOT NULL
+    DROP FUNCTION NumberOfStudentInBooking;
+GO
+
 CREATE FUNCTION dbo.NumberOfStudentInBooking(@bookingid as int)
 RETURNS int
 AS
@@ -13,6 +18,11 @@ END;
 GO
 
 -- 2. Get Person Type of a user
+
+IF OBJECT_ID (N'getUserType', N'FN') IS NOT NULL
+    DROP FUNCTION getUserType;
+GO
+
 CREATE FUNCTION dbo.getUserType(@UserID as int)
 RETURNS varchar(120)
 AS
@@ -23,5 +33,23 @@ BEGIN
     WHERE PersonTypeID = (SELECT PersonTypeID FROM dbo.Users WHERE UserID = @UserID)
 
     RETURN @UserType
+END;
+GO
+
+
+IF OBJECT_ID (N'getUserID', N'FN') IS NOT NULL
+    DROP FUNCTION getUserID;
+GO
+
+CREATE FUNCTION dbo.getUserID(@UserEmail as varchar(120))
+RETURNS int
+AS
+BEGIN
+    DECLARE @ID AS INT
+    SELECT @ID = U.UserID
+    FROM Users U
+    WHERE (U.Email = @UserEmail)
+
+    Return @ID
 END;
 GO

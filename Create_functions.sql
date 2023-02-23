14b996e7814c91ef8cbee962ef02262b4ffa02ad
Hewitt Nyambalo <122514230+hewitt-BBD@users.noreply.github.com>
2023-02-23 13:35:19 +0200

    Add files via upload

---
+++ Create_functions.sql
@@ -0,0 +1,1 @@
+--  1. Get Number of Students in Booking
---
+++ Create_views.sql
@@ -0,0 +1,7 @@
+--  1. Available Lessons (Parent Side)
+ 
+--  2. Booked Sessions (Parent Side)
+
+--  3. Booked Session (Student Side)
+
+--  4. Tutor's Available Sessions

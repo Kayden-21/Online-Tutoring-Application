fc686926dbafd6881cd207637b0c56f40fb09dfe
i-renaissant <hewitt.tny@gmail.com>
2023-02-24 08:30:46 +0200

    add sample data

---
+++ Usage.sql
@@ -0,0 +1,15 @@
+
+USE TutoringApplicationDB
+
+SELECT * FROM Users Where UserID = (dbo.getUserID('jeff@gmail.com'));
+GO
+
+SELECT * FROM Subject
+GO
+
+DECLARE @TutorID AS INT
+SELECT @TutorID = dbo.getUserID('jeff@gmail.com')
+
+EXEC dbo.createLesson @TutorID, 6, 10, '20120301 05:00:00 PM', '20120301 06:00:00 PM';
+
+SELECT * FROM dbo.Bookings Where TutorID = @TutorID
